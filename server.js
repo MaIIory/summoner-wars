7b5be3797c2c67565019b6e6cfc40cc529b2b5d6
unknown <maly_sky@o2.pl>
2014-03-12 22:11:45 +0100

    abc

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
 var express = require('express');
 var app = express.createServer(express.logger());
-var io = require('socket.io');
+var io = require('socket.io').listen(app);
 var port = process.env.PORT || 5000;
 
 app.listen(port);
