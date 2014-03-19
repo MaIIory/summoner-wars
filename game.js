2f9199eb4b12179656e5f83c6e20e06c8f2f151d
unknown <maly_sky@o2.pl>
2014-03-19 21:55:42 +0100

    sad

--- game.js
+++ game.js
@@ -1,86 +1,53 @@
-//<canvas id="canvas">
-//degrees = radians * (180/pi)
-//radians = degrees * (pi/180)
-//drawImage(Image Object, source X, source Y, source Width, source Height, destination X, destination Y, Destination width, Destination height)
-/*********************VARIABLES DECLARATION********************/
-//-----------------------------------------------------------//
+alert("asdasd");
 
-var ctx = null;
+var Clear = function () {
+    ctx.fillStyle = 'black'; //set active color
 
+    
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'white'; //set active color
+    ctx.fillText(cnt, width / 2, 200);
+    cnt = cnt + 1;
+    
+}
 
-
-/*************************DEFINE EVENTS*************************/
-//-----------------------------------------------------------//
-
-
-/***************************CLASSES****************************/
-//-----------------------------------------------------------//
-
-
-/***************************FUNCTIONS**************************/
-//-----------------------------------------------------------//
-
-var initGame = function ()
-   {
-   /*
-   
-   //main settings and game data
-   var width = 1200;    //canvas width
-   var height = 800;   //canvas height
-   var canvas = document.getElementById('canvas');
-   ctx = canvas.getContext('2d');
-
-   canvas.width = width;
-   canvas.height = height;
-   
-   
-   // requestAnim
+    //requestAnim
     window.requestAnimFrame = (function () {
         return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||                                */
-   //         function (/* function */ callback, /* DOMElement */ element) {
-   //             window.setTimeout(callback, 1000 / 50);
-   //         };
-   // })();
-   gameLoop();
-   }
+            window.msRequestAnimationFrame ||                                
+            function (/* function */ callback, /* DOMElement */ element) {
+                window.setTimeout(callback, 1000 / 50);
+            };
+    })();
+
+var cnt = 0;
+
+    var background_image = new Image();
+    background_image.src = "/img/background.jpg";
 
 
-
-
-
-var Clear = function () {
-    ctx.fillStyle = 'rgba(22, 138, 14, 1)'; //set active color
-    ctx.fillRect(0, 0, width, height);
-}
 
 
 /************************Main game loop************************/
 //-----------------------------------------------------------//
 var gameLoop = function () {
     //ctx.translate(0, 0);
+    
     Clear();
-
-    ctx.fillStyle = 'Black';
+    //ctx.fillStyle = 'Black';
     //ctx.unfont = '20pt Verdana';
-    ctx.textAlign = "center";
-    ctx.fillText("Game start", width / 2, 200);
+    //ctx.textAlign = "center";
+    //ctx.fillText("Game start", width / 2, 200);
     //ctx.fillText("mouse Y = " + ~~mouse_y, width / 2, 230);
     //ctx.fillText(Math.atan2(mouse_y - height / 2, mouse_x - width / 2), width / 2, 270);
     //ctx.fillText(bullets.length, width / 2, 300);
     //ctx.fillText(RadToDegree(Math.atan2(mouse_y - height / 2, mouse_x - width / 2)), width / 2, 330);
     //ctx.fillText(~~distance, width / 2, 200);
-
+    ctx.drawImage(background_image, 0, 0, 1024, 768, 0, 0, 1024, 768);
 
     requestAnimFrame(gameLoop);
 }
 
-//CALL OF GAME LOOP
-//---------------//
-
-//GameLoop();
-
-exports.initGame = initGame; //public function
