1ca08a6f471d693ede4930185f8013b008d92ccb
unknown <maly_sky@o2.pl>
2014-04-01 23:49:10 +0200

    asda

--- scripts/game.js
+++ scripts/game.js
@@ -8,7 +8,7 @@ var height = 768;   //canvas height and background image
 
 //load background image
 var background_image = new Image();
-background_image.src = "/img/background.jpg";
+background_image.src = "/../img/background.jpg";
 
 //game state
 var state = 0; /* 0 - menu
