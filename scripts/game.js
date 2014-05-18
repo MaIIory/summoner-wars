b062ec9742db6af7d685b0758b143a20e828574d
unknown <maly_sky@o2.pl>
2014-05-18 23:29:44 +0200

    Asd

--- scripts/game.js
+++ scripts/game.js
@@ -341,27 +341,47 @@ var Board = function () {
 
                 if ((that.matrix[i][j] != null) && (that.matrix[i][j].selected)) {
 
+                    //TODO replace 3 digit eith number of moves
+                    //check lowe places
                     for (var k = 1; k < 3; k++) {
 
-                        //check lowe places
                         if ((i + k) < that.matrix.length && that.matrix[i + k][j] === null) {
                             ctx.fillRect(that.s_x + (j * that.square_w), that.s_y + ((i + k) * that.square_h), that.square_w, that.square_h);
                         }
+                        else
+                            break;
+                    }
 
-                        //check upper places 
+                    //check upper places 
+                    for (var k = 1; k < 3; k++) {
+
                         if ((i - k) >= 0 && that.matrix[i - k][j] === null) {
                             ctx.fillRect(that.s_x + (j * that.square_w), that.s_y + ((i - k) * that.square_h), that.square_w, that.square_h);
                         }
+                        else
+                            break;
+                    }
 
-                        //check left places
+                    //check left places
+                    for (var k = 1; k < 3; k++) {
                         if ((j - k) > 0 && that.matrix[i][j - k] === null) {
                             ctx.fillRect(that.s_x + ((j - k) * that.square_w), that.s_y + (i * that.square_h), that.square_w, that.square_h);
                         }
-
-                        //check right places
-
-
+                        else
+                            break;
                     }
+
+                    //check right places
+                    for (var k = 1; k < 3; k++) {
+
+                        if ((j + k) < that.matrix[i].length && that.matrix[i][j + k] === null) {
+                            ctx.fillRect(that.s_x + ((j + k) * that.square_w), that.s_y + (i * that.square_h), that.square_w, that.square_h);
+                        }
+                        else
+                            break;
+                    }
+
+
                 }
             }
         }
