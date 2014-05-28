364a5073d390fb7d1f01113bf674ec064d9a91d2
unknown <maly_sky@o2.pl>
2014-05-28 21:57:49 +0200

    asdllll3

--- scripts/game.js
+++ scripts/game.js
@@ -397,22 +397,18 @@ var Board = function () {
                         ctx.fillStyle = "rgba(4, 124, 10, 0.4)";
                         ctx.fillRect(that.s_x + (j * that.square_w), that.s_y + (i * that.square_h), that.square_w, that.square_h);
 
-                        ctx.beginPath();
-                        ctx.moveTo(0, 0);
-                        ctx.lineTo(500, 500);
-                        ctx.stroke();
 
                         ctx.fillText('previous moves length: ' + that.matrix[card_i][card_j].previous_moves.length, 840, 620);
                         //draw previous moves
-                        for (var k = 0; k < that.matrix[card_i][card_j].previous_moves.length; k++) {
+                        for (var k = 0; k <= that.matrix[card_i][card_j].previous_moves.length; k++) {
                             
-                            ctx.fillStyle = "rgba(200, 124, 110, 0.45)";
+                            ctx.fillStyle = "rgba(200, 124, 110, 1)";
                             if (k === that.matrix[card_i][card_j].previous_moves.length) {
                                 ctx.beginPath();
-                                //ctx.moveTo(that.s_x + (that.matrix[card_i][card_j].previous_moves[k][1] * that.square_w), that.s_y + (that.matrix[card_i][card_j].previous_moves[k][0] * that.square_h));
-                                //ctx.lineTo(that.s_x + (card_i * that.square_w), that.s_y + (card_j * that.square_h));
-                                ctx.moveTo(0,0);
-                                ctx.lineTo(100, 100);
+                                ctx.moveTo(that.s_x + (that.matrix[card_i][card_j].previous_moves[k][1] * that.square_w), that.s_y + (that.matrix[card_i][card_j].previous_moves[k][0] * that.square_h));
+                                ctx.lineTo(that.s_x + (card_i * that.square_w), that.s_y + (card_j * that.square_h));
+                                //ctx.moveTo(0,0);
+                                //ctx.lineTo(100, 100);
                                 ctx.stroke();
                             } /*else {
                                 ctx.beginPath();
