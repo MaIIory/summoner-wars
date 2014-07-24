c9969f473009fe33e3275a2181ebc90a36520069
unknown <maly_sky@o2.pl>
2014-07-24 22:54:53 +0200

    asd

--- scripts/factions.js
+++ scripts/factions.js
@@ -12,35 +12,35 @@ var PheonixElves = function (player_name) {
     that.initDeck = function () {
 
         that.deck = [
-            /* new Card(name, id, src_x, src_y) */
-            new Card('Archer', 'pe03', 390, 0, that.player_name, 4),
-            new Card('Archer', 'pe04', 390, 0, that.player_name, 4),
-            new Card('Archer', 'pe05', 390, 0, that.player_name, 4),
-            new Card('Archer', 'pe06', 390, 0, that.player_name, 4),
-            new Card('Guardian', 'pe08', 520, 0, that.player_name, 1),
-            new Card('Guardian', 'pe09', 520, 0, that.player_name, 1),
-            new Card('Guardian', 'pe10', 520, 0, that.player_name, 1),
-            new Card('Guardian', 'pe11', 520, 0, that.player_name, 1),
-            new Card('Guardian', 'pe12', 520, 0, that.player_name, 1),
-            new Card('Warrior', 'pe14', 650, 0, that.player_name, 1),
-            new Card('Warrior', 'pe15', 650, 0, that.player_name, 1),
-            new Card('Warrior', 'pe16', 650, 0, that.player_name, 1),
-            new Card('Warrior', 'pe17', 650, 0, that.player_name, 1),
-            new Card('Warrior', 'pe18', 650, 0, that.player_name, 1),
-            new Card('Fire Drake', 'pe19', 0, 0, that.player_name, 1),
-            new Card('Kaeseeall', 'pe20', 130, 0, that.player_name, 3),
-            new Card('Maelena', 'pe21', 260, 0, that.player_name, 3),
-            new Card('A Hero Is Born', 'pe22', 0, 85, that.player_name, 0),
-            new Card('Burn', 'pe23', 130, 85, that.player_name, 0),
-            new Card('Burn', 'pe24', 130, 85, that.player_name, 0),
-            new Card('Greater Burn', 'pe25', 260, 85, that.player_name, 0),
-            new Card('Magic Drain', 'pe26', 390, 85, that.player_name, 0),
-            new Card('Magic Drain', 'pe27', 390, 85, that.player_name, 0),
-            new Card('Spirit of the Phoenix', 'pe28', 520, 85, that.player_name, 0),
-            new Card('Spirit of the Phoenix', 'pe29', 520, 85, that.player_name, 0),
-            new Card('Spirit of the Phoenix', 'pe30', 520, 85, that.player_name, 0),
-            new Card('Wall', 'pe33', 780, 0, that.player_name, 0),
-            new Card('Wall', 'pe34', 780, 0, that.player_name, 0)
+            /* new Card(name, id, src_x, src_y, owner_name, range, attack) */
+            new Card('Archer', 'pe03', 390, 0, that.player_name, 4, 1),
+            new Card('Archer', 'pe04', 390, 0, that.player_name, 4, 1),
+            new Card('Archer', 'pe05', 390, 0, that.player_name, 4, 1),
+            new Card('Archer', 'pe06', 390, 0, that.player_name, 4, 1),
+            new Card('Guardian', 'pe08', 520, 0, that.player_name, 1, 1),
+            new Card('Guardian', 'pe09', 520, 0, that.player_name, 1, 1),
+            new Card('Guardian', 'pe10', 520, 0, that.player_name, 1, 1),
+            new Card('Guardian', 'pe11', 520, 0, that.player_name, 1, 1),
+            new Card('Guardian', 'pe12', 520, 0, that.player_name, 1, 1),
+            new Card('Warrior', 'pe14', 650, 0, that.player_name, 1, 2),
+            new Card('Warrior', 'pe15', 650, 0, that.player_name, 1, 2),
+            new Card('Warrior', 'pe16', 650, 0, that.player_name, 1, 2),
+            new Card('Warrior', 'pe17', 650, 0, that.player_name, 1, 2),
+            new Card('Warrior', 'pe18', 650, 0, that.player_name, 1, 2),
+            new Card('Fire Drake', 'pe19', 0, 0, that.player_name, 1, 3),
+            new Card('Kaeseeall', 'pe20', 130, 0, that.player_name, 3, 2),
+            new Card('Maelena', 'pe21', 260, 0, that.player_name, 3, 2),
+            new Card('A Hero Is Born', 'pe22', 0, 85, that.player_name, 0, 0),
+            new Card('Burn', 'pe23', 130, 85, that.player_name, 0, 0),
+            new Card('Burn', 'pe24', 130, 85, that.player_name, 0, 0),
+            new Card('Greater Burn', 'pe25', 260, 85, that.player_name, 0, 0),
+            new Card('Magic Drain', 'pe26', 390, 85, that.player_name, 0, 0),
+            new Card('Magic Drain', 'pe27', 390, 85, that.player_name, 0, 0),
+            new Card('Spirit of the Phoenix', 'pe28', 520, 85, that.player_name, 0, 0),
+            new Card('Spirit of the Phoenix', 'pe29', 520, 85, that.player_name, 0, 0),
+            new Card('Spirit of the Phoenix', 'pe30', 520, 85, that.player_name, 0, 0),
+            new Card('Wall', 'pe33', 780, 0, that.player_name, 0, 0),
+            new Card('Wall', 'pe34', 780, 0, that.player_name, 0, 0)
         ]
     }
 
@@ -51,12 +51,12 @@ var PheonixElves = function (player_name) {
     that.getStartCards = function () {
 
         return [
-            [new Card('Warrior', 'pe13', 650, 0, that.player_name, 1), 0, 6],
-            [new Card('Guardian', 'pe07', 520, 0, that.player_name, 1), 1, 5],
-            [new Card('Wall', 'pe32', 780, 0, that.player_name, 0), 2, 5],
-            [new Card('Prince Elien', 'pe31', 650, 85, that.player_name, 3), 2, 7],
-            [new Card('Archer', 'pe01', 390, 0, that.player_name, 4), 3, 7],
-            [new Card('Archer', 'pe02', 390, 0, that.player_name, 4), 5, 6]
+            [new Card('Warrior', 'pe13', 650, 0, that.player_name, 1, 2), 0, 6],
+            [new Card('Guardian', 'pe07', 520, 0, that.player_name, 1, 1), 1, 5],
+            [new Card('Wall', 'pe32', 780, 0, that.player_name, 0, 0), 2, 5],
+            [new Card('Prince Elien', 'pe31', 650, 85, that.player_name, 3, 3), 2, 7],
+            [new Card('Archer', 'pe01', 390, 0, that.player_name, 4, 1), 3, 7],
+            [new Card('Archer', 'pe02', 390, 0, that.player_name, 4, 1), 5, 6]
         ]
     }
 
@@ -76,35 +76,35 @@ var TundraOrcs = function (player_name) {
     that.initDeck = function () {
 
         that.deck = [
-            /* new Card(name, id, src_x, src_y) */
-            new Card('Fighter', 'to03', 390, 0, that.player_name, 1),
-            new Card('Fighter', 'to04', 390, 0, that.player_name, 1),
-            new Card('Fighter', 'to05', 390, 0, that.player_name, 1),
-            new Card('Fighter', 'to06', 390, 0, that.player_name, 1),
-            new Card('Shaman', 'to08', 520, 0, that.player_name, 3),
-            new Card('Shaman', 'to09', 520, 0, that.player_name, 3),
-            new Card('Shaman', 'to10', 520, 0, that.player_name, 3),
-            new Card('Shaman', 'to11', 520, 0, that.player_name, 3),
-            new Card('Shaman', 'to12', 520, 0, that.player_name, 3),
-            new Card('Smasher', 'to14', 650, 0, that.player_name, 1),
-            new Card('Smasher', 'to15', 650, 0, that.player_name, 1),
-            new Card('Smasher', 'to16', 650, 0, that.player_name, 1),
-            new Card('Smasher', 'to17', 650, 0, that.player_name, 1),
-            new Card('Smasher', 'to18', 650, 0, that.player_name, 1),
-            new Card('Blagog', 'to19', 0, 0, that.player_name, 1),
-            new Card('Krung', 'to20', 130, 0, that.player_name, 1),
-            new Card('Ragnor', 'to21', 260, 0, that.player_name, 1),
-            new Card('Wall', 'to23', 520, 85, that.player_name, 0),
-            new Card('Wall', 'to24', 520, 85, that.player_name, 0),
-            new Card('Freeze', 'to26', 130, 85, that.player_name, 0),
-            new Card('Freeze', 'to27', 130, 85, that.player_name, 0),
-            new Card('Freeze', 'to28', 130, 85, that.player_name, 0),
-            new Card('Ice Wall', 'to29', 260, 85, that.player_name, 0),
-            new Card('Ice Wall', 'to30', 260, 85, that.player_name, 0),
-            new Card('Ice Wall', 'to31', 260, 85, that.player_name, 0),
-            new Card('Reinforcements', 'to32', 390, 85, that.player_name, 0),
-            new Card('Reinforcements', 'to33', 390, 85, that.player_name, 0),
-            new Card('A Hero Is Born', 'to34', 0, 85, that.player_name, 0)
+            /* new Card(name, id, src_x, src_y, owner_name, range, attack) */
+            new Card('Fighter', 'to03', 390, 0, that.player_name, 1, 1),
+            new Card('Fighter', 'to04', 390, 0, that.player_name, 1, 1),
+            new Card('Fighter', 'to05', 390, 0, that.player_name, 1, 1),
+            new Card('Fighter', 'to06', 390, 0, that.player_name, 1, 1),
+            new Card('Shaman', 'to08', 520, 0, that.player_name, 3, 2),
+            new Card('Shaman', 'to09', 520, 0, that.player_name, 3, 2),
+            new Card('Shaman', 'to10', 520, 0, that.player_name, 3, 2),
+            new Card('Shaman', 'to11', 520, 0, that.player_name, 3, 2),
+            new Card('Shaman', 'to12', 520, 0, that.player_name, 3, 2),
+            new Card('Smasher', 'to14', 650, 0, that.player_name, 1, 2),
+            new Card('Smasher', 'to15', 650, 0, that.player_name, 1, 2),
+            new Card('Smasher', 'to16', 650, 0, that.player_name, 1, 2),
+            new Card('Smasher', 'to17', 650, 0, that.player_name, 1, 2),
+            new Card('Smasher', 'to18', 650, 0, that.player_name, 1, 2),
+            new Card('Blagog', 'to19', 0, 0, that.player_name, 1, 5),
+            new Card('Krung', 'to20', 130, 0, that.player_name, 1, 3),
+            new Card('Ragnor', 'to21', 260, 0, that.player_name, 1, 2),
+            new Card('Wall', 'to23', 520, 85, that.player_name, 0, 0),
+            new Card('Wall', 'to24', 520, 85, that.player_name, 0, 0),
+            new Card('Freeze', 'to26', 130, 85, that.player_name, 0, 0),
+            new Card('Freeze', 'to27', 130, 85, that.player_name, 0, 0),
+            new Card('Freeze', 'to28', 130, 85, that.player_name, 0, 0),
+            new Card('Ice Wall', 'to29', 260, 85, that.player_name, 0, 0),
+            new Card('Ice Wall', 'to30', 260, 85, that.player_name, 0, 0),
+            new Card('Ice Wall', 'to31', 260, 85, that.player_name, 0, 0),
+            new Card('Reinforcements', 'to32', 390, 85, that.player_name, 0, 0),
+            new Card('Reinforcements', 'to33', 390, 85, that.player_name, 0, 0),
+            new Card('A Hero Is Born', 'to34', 0, 85, that.player_name, 0, 0)
         ]
     }
 
@@ -115,12 +115,12 @@ var TundraOrcs = function (player_name) {
     that.getStartCards = function () {
 
         return [
-            [new Card('Fighter', 'to01', 390, 0, that.player_name, 1), 5, 4],
-            [new Card('Fighter', 'to02', 390, 0, that.player_name, 1), 2, 6],
-            [new Card('Shaman', 'to07', 520, 0, that.player_name, 3), 4, 6],
-            [new Card('Smasher', 'to13', 650, 0, that.player_name, 1), 1, 5],
-            [new Card('Wall', 'to22', 520, 85, that.player_name, 0), 3, 5],
-            [new Card('Grognack', 'to25', 650, 85, that.player_name, 1), 3, 7]
+            [new Card('Fighter', 'to01', 390, 0, that.player_name, 1, 1), 5, 4],
+            [new Card('Fighter', 'to02', 390, 0, that.player_name, 1, 1), 2, 6],
+            [new Card('Shaman', 'to07', 520, 0, that.player_name, 3, 2), 4, 6],
+            [new Card('Smasher', 'to13', 650, 0, that.player_name, 1, 2), 1, 5],
+            [new Card('Wall', 'to22', 520, 85, that.player_name, 0, 0), 3, 5],
+            [new Card('Grognack', 'to25', 650, 85, that.player_name, 1, 4), 3, 7]
         ]
     }
 
