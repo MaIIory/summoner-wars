59e3d3c5c57c0059728e529cefec41b0f6bcd2f6
unknown <maly_sky@o2.pl>
2014-12-01 19:53:45 +0100

    sadsadasd

--- scripts/factions.js
+++ scripts/factions.js
@@ -116,7 +116,7 @@ var TundraOrcs = function (player_name) {
             [new Card('Shaman', 'to07', 520, 0, that.player_name, 3, 2, 2), 4, 6],
             [new Card('Smasher', 'to13', 650, 0, that.player_name, 1, 2, 4), 1, 5],
             //[new Card('Wall', 'to22', 520, 85, that.player_name, 0, 0, 9), 3, 5],
-            [new Card('Ice Wall', 'to31', 260, 85, that.player_name, 0, 0, 3), 3, 5]
+            [new Card('Ice Wall', 'to31', 260, 85, that.player_name, 0, 0, 3), 3, 5],
             [new Card('Grognack', 'to25', 650, 85, that.player_name, 1, 4, 7), 3, 7]
         ]
     }
