bf7fe3dd26eea478e5568489b676a670f008d4ab
unknown <maly_sky@o2.pl>
2015-01-07 20:25:00 +0100

    asdasd

--- scripts/game.js
+++ scripts/game.js
@@ -1538,15 +1538,17 @@ var gameLoop = function () {
         return;
     }
 
-    if ((mouse_button_down || (mouse_used === false)) && mouse_state === 0)
+    if ((mouse_button_down || (mouse_used === false)) && mouse_state === 0) {
         mouse_state = 1;
+    }
     else if (mouse_state === 1) {
         mouse_state = 2;
         mouse_used = true;
     }
-    else if (mouse_state === 2 && (mouse_button_down === false))
+    else if (mouse_state === 2 && (mouse_button_down === false)) {
         mouse_used = true;
         mouse_state = 0;
+    }
         
 
     if (state === 0) {
