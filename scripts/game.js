c1f30720ed121798ba3c56257a0bb864d95a0808
unknown <maly_sky@o2.pl>
2015-01-07 19:18:22 +0100

    asdasd

--- scripts/game.js
+++ scripts/game.js
@@ -54,7 +54,7 @@ var lastRun;
 //gameLoop data
 var previous = Date.now();
 var lag = 0.0;
-var MS_PER_UPDATE = 1;
+var MS_PER_UPDATE = 5;
 
 /*************************DEFINE EVENTS*************************/
 //-----------------------------------------------------------//
