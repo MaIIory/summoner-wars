65eeb3becd82adc504e96f627747841b01e1bf6f
unknown <maly_sky@o2.pl>
2015-02-05 00:34:17 +0100

    asas

--- scripts/game.js
+++ scripts/game.js
@@ -787,7 +787,6 @@ var PlaygroundHandler = function () {
             }
         }
 
-
         that.draw = function () {
 
             ctx.fillStyle = "rgba(233, 233, 233, 0.3)";
@@ -1409,11 +1408,11 @@ var PlaygroundHandler = function () {
                         if (that.card_container[i].selected) {
 
                             if (that.card_container[i].hover_eyeglass) {
-                                ctx.drawImage(parent.image, 0, that.board.sheet_origin, that.card_container[i].board_w, that.card_container[i].board_h,
+                                ctx.drawImage(parent.image, 0, parent.board.sheet_origin, that.card_container[i].board_w, that.card_container[i].board_h,
                                     that.current_pos + that.card_container_s_x, that.y + that.card_container_s_y + (i * that.card_container[i].board_h) + (i * that.gap_between_cards),
                                     that.card_container[i].board_w, that.card_container[i].board_h);
                             } else {
-                                ctx.drawImage(parent.image, 130, that.board.sheet_origin, that.card_container[i].board_w, that.card_container[i].board_h,
+                                ctx.drawImage(parent.image, 130, parent.board.sheet_origin, that.card_container[i].board_w, that.card_container[i].board_h,
                                     that.current_pos + that.card_container_s_x, that.y + that.card_container_s_y + (i * that.card_container[i].board_h) + (i * that.gap_between_cards),
                                     that.card_container[i].board_w, that.card_container[i].board_h);
                             }
